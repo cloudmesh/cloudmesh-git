771f03190a79c44b9518d3d45b186d2c2c727bcb
Gregor von Laszewski <laszewski@gmail.com>
2019-09-05 12:40:30 -0400

    add the git command line manual proposal

--- cloudmesh/git/command/git.py
+++ cloudmesh/git/command/git.py
@@ -16,31 +16,65 @@ class GitCommand(PluginCommand):
         ::
 
           Usage:
-                git --file=FILE
-                git list
+                git create issue BUNDLE FILE [--org=ORG]
+                git create issue --repo=REPO FILE [--org=ORG]
+                git create repo NAME FIRSTNAME LASTNAME GITHUBID [--org=ORG]
+                git create repo --file=repos.csv [--org=ORG]
+
 
           This command does some useful things.
 
           Arguments:
               FILE   a file name
+              ORG    [default: cloudmesh-community]
 
           Options:
-              -f      specify the file
+              --file   specify the file
+              --repo   the repository
+
+          Description:
+
+                The organization is set by default to
+                cloudmesh-community
+
+                git create issue BUNDLE FILE
+
+                   Create an issue in the given bundle.
+                   The bundle is defined in cloudmesh-installer
+                   The content of the issue is specified in the file
+
+                git create issue --repo=REPO FILE
+                   Create an issue in the given repos.
+                   Note that the repos is a string defined as
+                   cloudmesh.Parameter, E.g. fa19-516-[100-103,105]
+                   effects the repos ending with 100, 101, 102,
+                   103, and 105
+
+                   The bundle is defined in cloudmesh-installer
+
+                git create repo NAME FIRSTNAME LASTNAME GITHUBID
+                    creates the repo
+
+                git create repo --file=repos.csv
+                    creates repos from a file in csv format
+                    the format in th csv file is
+
+                    reponame,lastname,firstname,githubid
 
         """
-        arguments.FILE = arguments['--file'] or None
+        # arguments.FILE = arguments['--file'] or None
 
         VERBOSE(arguments)
 
-        m = Manager()
+        # m = Manager()
 
-        if arguments.FILE:
-            print("option a")
-            m.list(path_expand(arguments.FILE))
-
-        elif arguments.list:
-            print("option b")
-            m.list("just calling list without parameter")
-
+        # if arguments.FILE:
+        #    print("option a")
+        #    m.list(path_expand(arguments.FILE))
+        #
+        # elif arguments.list:
+        #    print("option b")
+        #    m.list("just calling list without parameter")
+        #
         Console.error("This is just a sample")
         return ""
