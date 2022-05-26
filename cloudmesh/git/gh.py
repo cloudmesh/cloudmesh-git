807b6e11dfa68892fc966fe1a9bf6471f7433b9d
J.P <stapmoshun@gmail.com>
2022-05-26 13:03:18 -0400

    implement --repo to file or to parametrized list separated by comma

--- cloudmesh/git/gh.py
+++ cloudmesh/git/gh.py
@@ -39,34 +39,36 @@ class Gh:
         line = f'<tr><th> repo </th><th> url </th><th> title </th><th> assignees</th><th> labels</th></tr>'
         result.append(line)
 
-        for entry in entries:
-            url = entry['url']
-            title = entry['title']
-            assignees = entry['assignees']
-            labels = entry['labels']
-            n = os.path.basename(url)
-            repo = os.path.dirname(url)
-            repo_url = os.path.dirname(repo)
-            repo_name = repo_url.replace("https://github.com", "")
-            entry['repo'] = f'<a href="{repo_url}"> {repo_name} </a>'
-            entry['url'] = f'<a href="{url}"> {n} </a>'
-            entry['title'] = f'<a href="{url}"> {title} </a>'
-            entry['assignees'] = ",".join(
-                [f'<a href="github.com/{assignee["login"]}"> {assignee["name"] or assignee["login"]} </a>'
-                 for assignee in assignees])
-            if len(labels) > 0:
-                entry['labels'] = ",".join([label['name'].replace("PRIORITY ", "") for label in labels])
-            else:
-                entry['labels'] = ""
-            line = f'<tr>'\
-                   f'<td> {entry["repo"]} </td>'\
-                   f'<td> {entry["url"]} </td>'\
-                   f'<td> {entry["title"]} </td>'\
-                   f'<td> {entry["assignees"]}</td>'\
-                   f'<td>{entry["labels"]}<td>'\
-                   '</tr>'
-            result.append(line)
-        result.append("</table>")
-        result = "\n".join(result)
-
+        if entries:
+            for entry in entries:
+                url = entry['url']
+                title = entry['title']
+                assignees = entry['assignees']
+                labels = entry['labels']
+                n = os.path.basename(url)
+                repo = os.path.dirname(url)
+                repo_url = os.path.dirname(repo)
+                repo_name = repo_url.replace("https://github.com", "")
+                entry['repo'] = f'<a href="{repo_url}"> {repo_name} </a>'
+                entry['url'] = f'<a href="{url}"> {n} </a>'
+                entry['title'] = f'<a href="{url}"> {title} </a>'
+                entry['assignees'] = ",".join(
+                    [f'<a href="github.com/{assignee["login"]}"> {assignee["name"] or assignee["login"]} </a>'
+                     for assignee in assignees])
+                if len(labels) > 0:
+                    entry['labels'] = ",".join([label['name'].replace("PRIORITY ", "") for label in labels])
+                else:
+                    entry['labels'] = ""
+                line = f'<tr>'\
+                       f'<td> {entry["repo"]} </td>'\
+                       f'<td> {entry["url"]} </td>'\
+                       f'<td> {entry["title"]} </td>'\
+                       f'<td> {entry["assignees"]}</td>'\
+                       f'<td>{entry["labels"]}<td>'\
+                       '</tr>'
+                result.append(line)
+            result.append("</table>")
+            result = "\n".join(result)
+        else:
+            result = None
         return result
